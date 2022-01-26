fa3b41da8e6b4e44c5e5a6aa17489ee7bf13d5f6
hyokyun <gyrbs22@gmail.com>
2022-01-26 12:22:36 +0900

    tree traversal

---
+++ treetraversal/treetraversal.py
@@ -0,0 +1,53 @@
+#트리순회
+
+#class
+#print 순서에 유의
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+#전위 순회(root>left>right)
+def pre_order(node):
+    print(node.data, end=' ')
+    if node.left_node != None:
+        pre_order(tree[node.left_node])
+    if node.right_node != None:
+        pre_order(tree[node.right_node])
+
+#중위 순회(left>root>right)
+def in_order(node):
+    if node.left_node != None:
+        in_order(tree[node.left_node])
+    print(node.data, end=' ')
+    if node.right_node != None:
+        in_order(tree[node.right_node])
+
+#후위 순회(left>right>root)
+def post_order(node):
+    if node.left_node != None:
+        post_order(tree[node.left_node])
+    if node.right_node != None:
+        post_order(tree[node.right_node])
+    print(node.data, end=' ')
+
+import sys
+n = int(input())
+tree = {}
+
+for i in range(n):
+    #node 개수 n개
+    data, left_node, right_node = map(str, sys.stdin.readline().split())
+    if left_node == "None":
+        left_node = None
+    if right_node == "None":
+        right_node = None
+    tree[data] = Node(data, left_node, right_node)
+
+#결과출력
+pre_order(tree['A'])
+print()
+in_order(tree['A'])
+print()
+post_order(tree['A'])
